9ad884d3f799ce4c183462e3dea699a00de64cbf
Umang Mathod <87117456+rockypagal@users.noreply.github.com>
2023-07-12 15:31:20 +0530

    half footer complete

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,6 +4,8 @@ import orangeRound from './img/beer-light-1.png'
 import history from  './img/brand-history.png'
 import three2 from './img/choose-flavor.png'
 import main from './img/hero-beer-bottles-3.png'
+import pub from './img/pub.jpg';
+import house from './img/house.jpg';
+import dark from './img/dark.jpg';
 
-
-export {blackRound, three1, three2, history, main ,orangeRound}
+export {blackRound, three1, three2,pub, history, main ,orangeRound,house,dark}
--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import './Footer.css' 
-
+import React from "react";
+import "./Footer.css";
+import { dark, house, pub } from "../../assets";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <section className="footerSection">
+      <div className="footer_heading">
+        <div className="footer_heading_cover">
+          {" "}
+          <h1 className="heading-font">
+            Discover The <br />
+            Brewing Process
+          </h1>
+          <p className="normal-font">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+            tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+          </p>
+        </div>
+      </div>
+      <div className="footer">
+        <div className="footer1">
+          <h1 className="heading-font">Hosting A Party?</h1>
+          <p className="normal-font">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+            tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+          </p>
+        </div>
+        <div className="footer2">
+          <h1 className="heading-font">Own a Pub or Bar?</h1>
+          <p className="normal-font">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+            tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+          </p>{" "}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Footer
+export default Footer;
